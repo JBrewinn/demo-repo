fa7fc93c22d2847b0bd96e20dc44d8f8f610e558
JBrewinn <jyow97@gmail.com>
2022-01-24 13:23:20 -0600

    commit

---
+++ demo.js
@@ -0,0 +1,43 @@
+// define UI variables
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listenters
+loadEventListeners();
+
+// Load all event liseners
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('Add a Task');
+    }
+    
+    //create li element
+    const li = document.createElement('li');
+    //add class
+    li.className = 'collection-item';
+    //create text node and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //create new link
+    const link = document.createElement('a');
+    //add class
+    link.className = 'delete-item secondary-content';
+    //add icon html
+    link.innerHTML = '<i class="fa fa-remove"></i>';
+    //append the link to li
+    li.appendChild(link);
+
+    //append li to ul
+    taskList.appendChild(li);
+
+    //clear input
+    taskInput.value = '';
+
+    e.preventDefault();
+}
