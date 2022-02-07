72de9d79732c90e154a29c8c8f2a48f7de41a7b6
JBrewinn <jyow97@gmail.com>
2022-02-07 17:09:28 -0600

    homework

---
+++ loophomework.js
@@ -0,0 +1,7 @@
+// Create a while loop that will print all the numbers from 1 to 250, console logging "ESKETIT" every 7 digits and "OOH" every 5 digits. Every multiple that is the same between 7 and 5, log "GUCCI GANG" 
+
+
+
+
+
+// Create a Do While loop that will have two fighters battle to see who wins (both start at 100 health and damage is randomized from 1 to 25 every iteration)
