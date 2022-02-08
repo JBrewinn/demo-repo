53f5c20d527841ec85bf23c4a2b5a8ae47be194e
JBrewinn <jyow97@gmail.com>
2022-02-07 22:09:15 -0600

    GUCCI GANG completed

--- loophomework.js
+++ loophomework.js
@@ -1,7 +1,17 @@
 // Create a while loop that will print all the numbers from 1 to 250, console logging "ESKETIT" every 7 digits and "OOH" every 5 digits. Every multiple that is the same between 7 and 5, log "GUCCI GANG" 
 
 
+// var i = 1;
 
-
+// while (i <= 250) {
+//     if (i%35 == 0) {
+//         console.log("GUCCI GANG");
+//     } else if (i%5 == 0) {
+//         console.log("OOH");
+//     } else if (i%7 == 0) {
+//         console.log("ESKETIT");
+//     } else console.log(i);
+//     i++;
+// }
 
 // Create a Do While loop that will have two fighters battle to see who wins (both start at 100 health and damage is randomized from 1 to 25 every iteration)
